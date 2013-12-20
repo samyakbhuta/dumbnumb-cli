1f8b966de7fc50182009d28cb3c033d5b68cd773
Samyak Bhuta <samyak.bhuta@gmail.com>
2013-12-21 00:45:57 +0530

    Adds a Number.
    - Takes the input from command line.
    - Accesses the API and POST the request.
    - The server URL is hardcoded. It is local and listens on 1729.

---
+++ dumbnumb-add.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+var program = require('commander');
+var DumbNumb = require('./lib/dumbnumb-lib');
+var n = new DumbNumb();
+
+exports.command = {
+  description: 'Adds a number with it\'s description(s)'
+};
+
+if (require.main === module) {
+
+    program
+    .version('0.0.1')
+    .option('-n, --number <number>', 'A number to be added.\t\t\t\t*Required*')
+    .option('-d, --desc <description>', 'Description(s) for the number to be added.\t*Required*')
+    .parse(process.argv);
+
+    if (!program.number) {
+        console.log('');
+        console.log('  Oops ! Number is required.');
+    	program.help(); // will exit as well.
+    }
+
+    if (!program.desc) {
+        console.log('');
+        console.log('  Oops ! Description is required.');
+    	program.help(); // will exit as well.
+    }
+
+    n.post(program.number); 
+}
---
+++ dumbnumb.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+var helmsman = require('helmsman');
+helmsman().parse();
